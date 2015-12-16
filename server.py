0f824296d4eef6e78d6f017e64477b52be2cc4b4
pgowdy <pgowdy1@gmail.com>
2015-12-16 16:23:08 -0600

    Testing Template

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+
+# Initilization
 app = Flask(__name__)
+app.config.update(
+    DEBUG = True,
+)
 
+# Controllers
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
+# Launch to Heroku
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
